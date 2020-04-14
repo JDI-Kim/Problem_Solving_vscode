b84c1c43b6106453dd0e1d1b02de30d71875ab5d
Jisu Kim <gorae.k0@gmail.com>
2020-04-14 11:43:55 +0900

    use character array instead of integer array to reduce the time

--- N_and_M/nm1.cpp
+++ N_and_M/nm1.cpp
@@ -5,15 +5,18 @@ const int maxM=8;
 int N;
 int M;
 bool visit[maxN+1];
-int num[maxM+1];
+//int num[maxM+1];
+char arr[(maxM+1)*2];
 
 void read(){
     cin>>N>>M;
-    cout<<N<<" "<<M<<"\n";
 }
 void init(){
     for(int i=1;i<=N;++i){
         visit[i]=false;
+    }
+    for(int i=0;i<M*2;++i){
+        arr[i]=' ';
     }
 }
 void printresult(){
@@ -22,21 +25,27 @@ void printresult(){
     }
     cout<<"\n";
 }
+void printarr(){
+    cout<<arr<<"\n";
+}
 void solve(int count){
    if(count==M){
-       printresult();
+       //printresult();
+       printarr();
        return;
    }
    for(int i=1;i<=N;++i){
        if(visit[i]) continue;
        visit[i]=true;
-       num[count]=i;
+       //num[count]=i;
+       arr[count*2]=i+'0';
        solve(count+1);
        visit[i]=false;
    }
 }
 int main(void){
     ios::sync_with_stdio(false);
+    cin.tie(0);
     int Test;
     cin>>Test;
     for(int test=1;test<=Test;++test){
