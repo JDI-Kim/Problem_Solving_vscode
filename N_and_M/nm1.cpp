8bff72fd8df3191dca244c294db29b45f6653b45
Jisu Kim <gorae.k0@gmail.com>
2020-04-14 10:56:57 +0900

    중복 없는 수열 N개를 중복 없이 M번 고르기

--- N_and_M/nm1.cpp
+++ N_and_M/nm1.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 using namespace std;
+const int maxN=8;
+const int maxM=8;
+int N;
+int M;
+bool visit[maxN+1];
+int num[maxM+1];
+
+void read(){
+    cin>>N>>M;
+    cout<<N<<" "<<M<<"\n";
+}
+void init(){
+    for(int i=1;i<=N;++i){
+        visit[i]=false;
+    }
+}
+void printresult(){
+    for(int i=0;i<M;++i){
+        cout<<num[i]<<" ";
+    }
+    cout<<"\n";
+}
+void solve(int count){
+   if(count==M){
+       printresult();
+       return;
+   }
+   for(int i=1;i<=N;++i){
+       if(visit[i]) continue;
+       visit[i]=true;
+       num[count]=i;
+       solve(count+1);
+       visit[i]=false;
+   }
+}
 int main(void){
-    cout<<"HELLO"<<"\n";
+    ios::sync_with_stdio(false);
+    int Test;
+    cin>>Test;
+    for(int test=1;test<=Test;++test){
+        read();
+        init();
+        
+        cout<<"#"<<test<<"\n";
+        solve(0);
+        cout<<"\n\n";
+    }
     return 0;
 }
