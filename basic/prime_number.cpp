4601573d35cdf76b5cb19dcdab96037a1fd51761
Jisu Kim <gorae.k0@gmail.com>
2020-04-15 14:05:46 +0900

    string.at()은 접근이 느림

--- basic/prime_number.cpp
+++ basic/prime_number.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 int N;
 const int maxN=1000000;
-
+int cmp[7];
 void read(){
     cin>>N;
+    for(int i=0;i<7;++i){
+        cmp[i]=0;
+    }
 }
 bool isPrime(int n){
+    int root=(int)sqrt(n);
     if(n%2==0) return false;
-    for(int i=3;i<n;i+=2){
+    for(int i=3;i<=root;i+=2){
         if(n%i==0) return false;
     }
     return true;
@@ -18,15 +23,21 @@ bool isPrime(int n){
 
 bool isPal(int num){
     if(num<10) return true;
-    string str=to_string(num);
-    int size=str.size();
-    for(int i=0;i<=(size/2);++i){
-        if(str.at(i)!=str.at(str.size()-i-1)) return false;
+    int size=0;
+    for(size=0;num>0;++size){
+        cmp[size]=(num%10);
+        num=(int)num/10;
     }
-    return true;
+    int halfsize=size/2;
+    int size_1=size-1;
+    for(int i=0;i<=halfsize;++i){
+        if(cmp[i]!=cmp[size_1-i])return false;
+    }
+    return true;    
 }
 int solve(){
     int num;
+    if(N==1){return 2;}//1은 소수가 아니래...
     if(N<=3){return N;}
     if(N%2==0) num=N+1;
     else num=N;
