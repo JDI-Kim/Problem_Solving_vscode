60aa0caddf88074c02fc4ca00754e3a6e9a67129
Jisu Kim <gorae.k0@gmail.com>
2020-04-15 12:44:24 +0900

    중복 되고 순서 없는 뽑기

--- N_and_M/nm11.cpp
+++ N_and_M/nm11.cpp
@@ -1,16 +1,14 @@
-
 /*
-BOJ #15664. N과 M (10) 순서 없고 비내림차순
+BOJ #15665. N과 M (11) 순서 없고 비내림차순
 N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
 
 - N개의 자연수 중에서 M개를 고른 수열
-- 고른 수열은 비내림차순이어야 한다.
-    길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
+- 같은 수를 여러 번 골라도 된다.
 
 Imput
 - 첫째 줄에 테스트케이스 개수 T가 주어진다
 - 둘째 줄부터 각 테스트 케이스가 주어진다.
-- 각 테스트케이스 첫째줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+- 각 테스트케이스 첫째줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 7)
 - 각 테스트케이스 둘째 줄에 N개의 수가 주어진다. 입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다.
 
 Output
@@ -29,13 +27,14 @@ sample_input.txt
 */
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 int N,M;
-const int maxN=8;
-const int maxM=8;
+const int maxN=7;
+const int maxM=7;
 int input[maxN];
 int output[maxM];
-bool visit[maxN];
+
 void read(){
     cin>>N>>M;
     for(int i=0;i<N;++i){
@@ -44,9 +43,6 @@ void read(){
 }
 void init(){
     sort(input,input+N);
-    for(int i=0;i<N;++i){
-        visit[i]=false;
-    }
 }
 void printResult(){
     for(int i=0;i<M;++i){
@@ -54,33 +50,30 @@ void printResult(){
     }
     cout<<"\n";
 }
-void solve(int count, int startI){
-    if(count ==M){
+void solve(int count){
+    if(count==M){
         printResult();
-        return ;
+        return;
     }
     int pre = -1;
-    for(int i=startI;i<N;++i){
-        if(visit[i]||pre==input[i]) continue;
+    for(int i=0;i<N;++i){
+        if(pre==input[i])continue;
         output[count]=input[i];
-        visit[i]=true;
         pre=input[i];
-        solve(count+1,i+1);
-        visit[i]=false;
+        solve(count+1);
     }
-
 }
+
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(0);
     int Test;
     cin>>Test;
-    for(int test=1 ; test<=Test; ++test){
+    for(int test=1;test<=Test;++test){
         read();
         init();
-
         cout<<"#"<<test<<" "<<"\n";
-        solve(0,0);
+        solve(0);
     }
     return 0;
 }
