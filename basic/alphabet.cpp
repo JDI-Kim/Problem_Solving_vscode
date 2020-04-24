7161c1c214f28b2aaa889ee9b5ae6a82b216511f
Jisu Kim <gorae.k0@gmail.com>
2020-04-24 19:46:38 +0900

    BOJ #1987 알파벳; DFS 이게 더 빠른데.. 왜 더 빠른가?

--- basic/alphabet.cpp
+++ basic/alphabet.cpp
@@ -1,58 +1,65 @@
 /*
-    BOJ #1987 알파벳; DFS
+    BOJ #1987 알파벳; DFS a bit optimized
 */
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 int R,C;
-const int maxR=20+1;
-const int maxC=20+1;
-char board[maxR][maxC];
-typedef struct{
-    int r,c;
-}pos;
-pos mal;
+const int maxR=20+2;
+const int maxC=20+2;
+char board[maxR][maxC]={0,};
 int dx[4]={0,0,1,-1};
 int dy[4]={1,-1,0,0};
-bool visit[30];
+bool visit[200];
 int result;
 
 void read(){
     cin>>R>>C;
-    for(int i=0;i<R;++i){
-        cin>>board[i];
+    for(int i=0;i<=R+1;++i){
+       for(int j=0;j<=C+1;++j){
+           board[i][j]=0;
+       }
+    }
+    for(int i=1;i<=R;++i){
+        cin>>&(board[i][1]);
     }
 }
 void init(){
-    mal.r=0;
-    mal.c=0;
     result=1;
-    for(int i=0;i<30;++i){
+    for(int i=0;i<200;++i){
         visit[i]=false;
     }   
 }
-void findmax(int count, pos pre){
-    int charNum;
-    for(int d=0;d<4;++d){
-        int nR=pre.r+dy[d];
-        int nC=pre.c+dx[d];
-        if(nR<0||nR>=R||nC<0||nC>=C)continue;
-        charNum=board[nR][nC]-'A';
-        if(visit[charNum]) continue;
-        visit[charNum]=true;
-        int nCount=count+1;
-        result=max(result,nCount);
-        pos next={nR,nC};
-        findmax(nCount,next);
-        visit[charNum]=false;
+void findmax(int r, int c,int count){
+    if(result<count)result=count;
+    
+    if(!visit[board[r+1][c]]){
+        visit[board[r+1][c]]=true;
+        findmax(r+1,c,count+1);
+        visit[board[r+1][c]]=false;
     }
+    if(!visit[board[r-1][c]]){
+        visit[board[r-1][c]]=true;
+        findmax(r-1,c,count+1);
+        visit[board[r-1][c]]=false;
+    }
+    if(!visit[board[r][c+1]]){
+        visit[board[r][c+1]]=true;
+        findmax(r,c+1,count+1);
+        visit[board[r][c+1]]=false;
+    }
+    if(!visit[board[r][c-1]]){
+        visit[board[r][c-1]]=true;
+        findmax(r,c-1,count+1);
+        visit[board[r][c-1]]=false;
+    }
+
 }
 void solve(){
 
-    int charNum=board[mal.r][mal.c]-'A';
-    visit[charNum]=true;
-    findmax(1,mal);
+    visit[board[1][1]]=true;
+    visit[0]=true;
+    findmax(1,1,1);
 }
 int main(void){
     ios::sync_with_stdio(false);
