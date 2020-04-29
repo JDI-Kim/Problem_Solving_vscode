352e8127f10e12de6debe1da2af05726bcced73c
Jisu Kim <gorae.k0@gmail.com>
2020-04-30 02:30:48 +0900

    BOJ #3649 로봇프로젝트 *테스트케이스 갯수가 주어지지 않은 경우 while(cin>>a)를 쓰면됨 *정렬과 이진탐색. 예외처리할때는 주의할 것

--- basic/robot.cpp
+++ basic/robot.cpp
@@ -1,56 +1,71 @@
+/*
+    BOJ #3649 로봇프로젝트 *테스트케이스 갯수가 주어지지 않은 경우 while(cin>>a)를 쓰면됨
+    *정렬과 이진탐색. 절반인 값이 들어왔을 때 대처
+*/
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int  x,n;
-const int maxX=20+1;
-const int maxN=1000000+1;
-int l[maxN];
-int diff;
-int l1,l2;
+int x,n;
+const int maxN=1000000;
+int l1[maxN];
+int l2[maxN];
+int cnt1,cnt2;
+int ans1,ans2;
+int halfcnt;
 void read(){
-    int cnt=0;
-    diff=l1=l2=-1;
     cin>>n;
-
-    x *=10000000;
-   
+    x*=10000000;
     int temp;
+    cnt1=cnt2=0;
+    ans1=ans2=-1;
+    int half=x/2;
+    halfcnt=0;
     for(int i=0;i<n;++i){
-        cin>>temp;
-        if(temp>x) continue;
-        l[cnt]=temp;
-        
-        for(int j=0;j<cnt;++j){
-            if(l[cnt]+l[j]!=x)continue;
-            int tempDiff =abs(l[cnt]-l[j]);
-            if(diff>=tempDiff)continue;
-            l1=l[j];
-            l2=l[cnt];
-            diff=tempDiff;                
-        }
-        cnt++;
-    }
-    if(diff==-1) cout<<"danger"<<"\n";
-    else{
-        if(l2<l1){int num=l1; l1=l2;l2=num;}
-        cout<<"yes "<<l1<<" "<<l2<<"\n";
-    }
-}
 
+        cin>>temp;
+        if(temp>x){ continue; }
+        
+        if(temp<half) l1[cnt1++]=temp;
+        else if(temp>half) l2[cnt2++]=temp;
+        else halfcnt++;
+    }
+    if(halfcnt>=2){l1[cnt1++]=half;l2[cnt2++]=half;}
+    sort(l1,l1+cnt1);
+    sort(l2,l2+cnt2);
+}
+bool search(int goal,int first, int last){
+    if(first>last) return false;
+    int mid=(first+last)/2;
+    if(l2[mid]==goal) return true;
+    if(l2[mid]<goal) return search(goal,mid+1,last);
+    if(goal<l2[mid]) return search(goal,first,mid-1);
+    return false;
+}
+bool solve(){
+    int goal;
+    if(n==0) return false;
+   
+    for(int i=0;i<cnt1;++i){
+        goal=x-l1[i];
+        if(search(goal,0,cnt2-1)){
+            ans1=l1[i];
+            ans2=goal;
+            return true;
+        }
+    }
+    return false;
+}
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(0);
-    
-    while(cin>>x)
-    {        
+    while(cin>>x){
         read();
+        if(solve()){
+            cout<<"yes "<<ans1<<" "<<ans2<<"\n";}
+        else{
+            cout<<"danger"<<"\n";
+        }
+
     }
-    // int Test;
-    // cin>>Test;
-    // for(int test=1;test<=Test;++test){
-        
-    //     cout<<"#"<<test<<" "<<"\n";
-    //     read();
-    // }
     return 0;
 }
