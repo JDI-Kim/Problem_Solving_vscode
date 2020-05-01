55dc5fdf62e95c91a8955085e1e4f800ad5a23f2
Jisu Kim <gorae.k0@gmail.com>
2020-05-01 17:41:02 +0900

    BOJ #1700. 멀티탭 스케쥴링. **나중에 쓸 것 부터 빼면 됨.. 많이 쓰는걸 두는 게 아니라.

--- basic/multitab.cpp
+++ basic/multitab.cpp
@@ -1,65 +1,77 @@
-#include <iostream>
+/*
+    BOJ #1700. 멀티탭 스케쥴링. **나중에 쓸 것 부터 빼면 됨.. 많이 쓰는걸 두는 게 아니라.
+*/
 
+#include <iostream>
+#include <queue>
 using namespace std;
 int N, K;
-const int maxN=100;
-const int maxK=100;
+const int maxN=100+1;
+const int maxK=100+1;
+
 int input[maxK];
-int minResult;
-bool isPlugged[maxK+1];
-int plug[maxN];
-int numPlug;
+int nxt[maxK][maxK];
+int cnt[maxK];
+int num[maxK];
+int plugNum;
+bool isPlugged[maxK];
+int result=0;
+
+void init(){
+    plugNum=0;//nothing is plugged
+    result=0;
+    for(int i=0;i<=K;++i){
+        isPlugged[i]=false;
+        cnt[i]=0;
+        num[i]=0;
+        for(int j=0;j<=K;++j){
+            nxt[i][j]=0;
+        }
+    }
+}
 void read(){
     cin>>N>>K;
+    init();
     for(int i=0;i<K;++i){
         cin>>input[i];
-    }
+        nxt[input[i]][cnt[input[i]]++]=i;
+    }   
+    
 }
-void init(){
-    minResult=1000;
-    numPlug=0;
+int findSmallCNT(){
+    int minName=-1;
+    int uselater=-1;
     for(int i=1;i<=K;++i){
-        isPlugged[i]=false;
-    }
-    for(int i=0;i<N;++i){
-        plug[i]=0;
-    }
-}
-int findInput(int startI){
-    for(int i=startI;i<K;++i){
-        if(isPlugged[input[i]])continue;
-        return i;
-    }
-    return -1;
-}
-void solve(int index, int unpplug){
-    int I=findInput(index);
-    if(I==-1){
-        if(minResult>unpplug){
-            minResult=unpplug;
+        if(!isPlugged[i])continue;
+        if(cnt[i]==num[i]){return i;}
+        if(nxt[i][num[i]]>uselater){
+            uselater=nxt[i][num[i]];
+            minName=i;
         }
-        return;
     }
-    if(numPlug<N) {
-        isPlugged[input[I]]=true;
-        plug[numPlug]=input[I];
-        numPlug++;
-        solve(I+1, unpplug);
-        return;
-    }
-   
-    for(int i=0;i<N;++i){
-        int p=plug[i];
-        isPlugged[input[I]]=true;
-        isPlugged[p]=false;
-        plug[i]=input[I];
-        solve(I+1,unpplug+1);
-        plug[i]=p;
-        isPlugged[p]=true;
-        isPlugged[input[I]]=false;
-    }
+    return minName;
 }
-
+void solve(){
+    if(K==1) return;
+    if(N>=K) return;
+    int firstFull=0;
+    for(int i=0;i<K;++i){
+        num[input[i]]++;
+        if(isPlugged[input[i]]) continue;
+        isPlugged[input[i]]=true;       
+        plugNum++;
+        if(plugNum==N){ firstFull = i; break;}
+    }
+    for(int i=firstFull+1; i<K;++i){
+        num[input[i]]++;
+        if(isPlugged[input[i]]) continue;
+        int temp=findSmallCNT();
+        isPlugged[temp]=false;
+        isPlugged[input[i]]=true; 
+        result++;
+    }
+    
+}
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -67,9 +79,8 @@ int main(void){
     cin>>Test;
     for(int test=1;test<=Test;++test){
         read();
-        init();
-        solve(0,0);
-        cout<<"#"<<test<<" "<<minResult<<"\n";
+        solve();
+        cout<<"#"<<test<<" "<<result<<"\n";
     }
     return 0;
 }
