2868118f15d835c5f709d64fc61ebaed680006d8
Jisu Kim <gorae.k0@gmail.com>
2020-05-09 01:39:39 +0900

    [programmers]  2019 카카오 개발자 겨울 인턴십 불량 사용자 **index 주의.. *set을 쓸 줄 알면 좋음.

--- KAKAO/kakao2019_banned.cpp
+++ KAKAO/kakao2019_banned.cpp
@@ -1,71 +1,62 @@
 /*
    
     [programmers]  2019 카카오 개발자 겨울 인턴십 불량 사용자 **index 주의..
+    * set을 쓸 줄 알면 좋구나....
     출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
 
 */
+#include<iostream>
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
-void makeList(int count, int nB,int nU, vector<vector<int>> candidates, vector<bool> visit,vector<vector<bool>> &pList){
-    if(count==nB){
-        //중복체크하고 
-        int sz=pList.size();
-        int result=0;
-        bool possible=true;
-        for(int i=0;i<sz;++i){
-            result=0;
-            for(int j=0;j<nU;++j){
-                if(pList[i][j]!=visit[j]){ result++;break;}//다른게 있으면 가능.
-            }
-            if(result==0) {possible=false;break;}//같았으면 불가능.
-        }
-        if(possible) pList.push_back(visit);
-        return;
-    }
-    for(int i=0;i<candidates[count].size();++i){
-        if(visit[candidates[count][i]]) continue;
-        visit[candidates[count][i]]=true;
-        makeList(count+1,nB,nU,candidates,visit,pList);
-        visit[candidates[count][i]]=false;
-        
-    }
-}
+
 bool canbeBan(string u, string b){
-    int b_sz=b.size();
-    int u_sz=u.size();
-    if(u_sz!=b_sz) return false;
-    for(int i=0;i<b_sz;++i){
-        if(b[i]=='*') continue;
-        if(b[i]!=u[i]) return false;
+    if(u.size()!=b.size()) return false;
+    for(int i=0;i<b.size();++i){
+        if(b[i]=='*' || b[i]==u[i]) continue;
+        return false;
     }
     return true;
 }
+void makeList(int count, vector<bool>visit, vector<vector<int> > cand, set<string> &pList){
+    if(count==cand.size()){
+         bool result=false;
+         string str="";
+        for(int i=0;i<visit.size();++i){
+            str+=visit[i]+'0';
+        }
+        if(pList.find(str)==pList.end()) pList.insert(str);
+        return;
+    }
+    for(int i=0;i<cand[count].size();++i){
+        if(visit[cand[count][i]]) continue;
+        visit[cand[count][i]]=true;
+        makeList(count+1,visit,cand,pList);
+        visit[cand[count][i]]=false;
+    }
+}
 int solution(vector<string> user_id, vector<string> banned_id) {
     int answer = 0;
-    int nU=user_id.size();
-    int nB=banned_id.size();
-    vector<vector<bool>> pList;
-    vector<bool> visit;
-    vector<vector<int>>  candidates;
-    for(int i=0;i<nU;++i){
+    vector<vector<int> > cand;
+    set<string> pList;
+    vector<bool>visit;
+    for(int i=0;i<user_id.size();++i){
         visit.push_back(false);
     }
-    for(int b=0;b<nB;++b){
-        vector<int> v;        
-        for(int u=0;u<nU;++u){
-            if(canbeBan(user_id[u],banned_id[b])){
-                v.push_back(u);
+    for(int i=0;i<banned_id.size();++i){
+        vector<int> v;
+        for(int j=0;j<user_id.size();++j){
+            if(canbeBan(user_id[j],banned_id[i])){
+                v.push_back(j);
             }
         }
-        candidates.push_back(v);
+        cand.push_back(v);
     }
-    makeList(0,nB,nU,candidates,visit,pList);
-    
+
+    makeList(0,visit,cand,pList);
     answer=pList.size();
-    
-    
     return answer;
 }
 
